0a92c7502e6c0db7ef71ee1455200bb313ddaa58
BENARIBA-Iheb <96581759+b-iheb@users.noreply.github.com>
2023-01-03 00:40:54 +0100

    Update scaner.py

--- scaner.py
+++ scaner.py
@@ -16,29 +16,76 @@ resp = input("""\nPlease enter the type of scan you want to run
 
 print("You have selected option: ", resp)
 
+#////////////////////////////////////////////////////////////////////////////////////////
+
 if resp == '1':
     print("TCP(SYN ACK) scan: ")
     scanner.scan(ip_addr ,  "1-1024" , "-v -sS" , True) 
+    os_matches = scanner[ip_addr]['osmatch']
+    hostname = scanner[ip_addr].hostname()
+    
+    print(f"The hostname of the target host is: {hostname}")
+    
+    for os_match in os_matches:
+        print(f"Name: {os_match['name']}")        
+        has_wifi = False
+             for os_class in os_match['osclass']:
+                if os_class['type'] == 'WAP':
+                  has_wifi = True
+                  break
+                  
     print(scanner.scaninfo())
     print("ip Status: ",scanner[ip_addr].state())
     print(scanner[ip_addr].all_protocols())
     print("open ports: ", scanner[ip_addr]['tcp'].keys())
 
+#////////////////////////////////////////////////////////////////////////////////////////
+    
 elif resp == '2':
     print("UDP scan: ")
     scanner.scan(ip_addr , "1-1024" , "-v -sU" , True) 
+    os_matches = scanner[ip_addr]['osmatch']
+    hostname = scanner[ip_addr].hostname()
+        
+    print(f"The hostname of the target host is: {hostname}")
+    
+    for os_match in os_matches:
+        print(f"Name: {os_match['name']}")        
+        has_wifi = False
+             for os_class in os_match['osclass']:
+                if os_class['type'] == 'WAP':
+                  has_wifi = True
+                  break
+        
     print(scanner.scaninfo())
     print("ip Status: ",scanner[ip_addr].state())
     print(scanner[ip_addr].all_protocols())
     print("open ports: ", scanner[ip_addr]['udp'].keys())
 
+#////////////////////////////////////////////////////////////////////////////////////////
+
 elif resp == '3':
     print("total scan: ")
     scanner.scan(ip_addr , "1-1024" , "-v -sS -sV -sC -A -O" , True) 
+    os_matches = scanner[ip_addr]['osmatch']
+    hostname = scanner[ip_addr].hostname()
+        
+    print(f"The hostname of the target host is: {hostname}")
+    
+    for os_match in os_matches:
+        print(f"Name: {os_match['name']}")        
+        has_wifi = False
+            for os_class in os_match['osclass']:
+                if os_class['type'] == 'WAP':
+                    has_wifi = True
+                    break
+    
     print(scanner.scaninfo())
     print("ip Status: ",scanner[ip_addr].state())
     print(scanner[ip_addr].all_protocols())
     print("open ports: ", scanner[ip_addr]['tcp'].keys())
+
+#////////////////////////////////////////////////////////////////////////////////////////
 
 else:
     print("please enter one of the specefied scanning types.")
